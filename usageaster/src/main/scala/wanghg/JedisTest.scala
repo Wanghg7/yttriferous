233f90052d0f8c9103a29abfe0bcd95c7336b3f8
wanghg <wanghg7@gmail.com>
2017-04-20 17:45:13 +0800

    jedis ok

---
+++ usageaster/src/main/scala/wanghg/JedisTest.scala
@@ -0,0 +1,16 @@
+package wanghg
+
+import redis.clients.jedis.Jedis
+
+/**
+  * Created by wanghg on 20/4/2017.
+  */
+object JedisTest {
+
+  def main(args: Array[String]): Unit = {
+    val jedis = new Jedis("localhost")
+    val value = jedis.get("wanghg")
+    println(value)
+  }
+
+}
